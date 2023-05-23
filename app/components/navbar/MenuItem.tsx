99e4807936c36c891b5b24cfb26acc5fb0f5e40f
NKolosov097 <N.Kolosov2003@mail.ru>
2023-05-23 22:36:09 +0300

    init: Airbnb-clone project; add: navbar

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+interface IMenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem = ({ onClick, label }: IMenuItemProps): JSX.Element => {
+  return (
+    <div
+      className="
+    px-4
+    py-3
+    hover:bg-neutral-100
+    transition
+    font-semibold
+  "
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItem;
